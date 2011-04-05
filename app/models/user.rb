a125024a0d02ce1e73c21289da3607f737ea71dc
Joan Wolkerstorfer <joanwolk@gmail.com>
2011-04-05 15:59:41 +0200

    Updated user model annotation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 # == Schema Information
-# Schema version: 20110301160443
+# Schema version: 20110307155353
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
 require 'digest'
