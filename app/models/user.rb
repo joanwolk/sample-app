56e923534afb58ddc84d19b4812937447c7c42ce
Joan Wolkerstorfer <joanwolk@gmail.com>
2011-05-26 16:38:25 +0200

    Added admin privilege to delete users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20110307155353
+# Schema version: 20110525123217
 #
 # Table name: users
 #
@@ -10,6 +10,7 @@
 #  updated_at         :datetime
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  admin              :boolean
 #
 
 require 'digest'
